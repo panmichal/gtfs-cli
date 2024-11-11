558db43056cb1a902e2d6bb61fe74f1b6babd49f
Michał Strzelczyk <817457+panmichal@users.noreply.github.com>
2024-11-11 10:02:11 +0100

    Create basic cli app with Cobra

---
+++ gtfs.go
@@ -0,0 +1,5 @@
+package gtfs-cli
+
+func Hello() string {
+    return "Hello, world."
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/panmichal/gtfs-cli/cmd"
+
+func main() {
+	cmd.Execute()
+}
